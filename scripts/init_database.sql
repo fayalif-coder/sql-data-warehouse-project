f96f361fc199637b11edc322b932e9ced01ea5f8
fayalif-coder <fayalif@gmail.com>
2025-06-02 13:05:52 +0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,159 @@
+/*
+------------
+ WARNING:
+------------
+ Following Script will drop the Database and will recreate the Database.
+ Make sure to backup your existing database before running this script.
+ Moreover, please set the path for your database files according to your
+ system's local path. Search for this "[Local System Path]" to find and change
+ the path accordingly.
+*/
+
+USE [master]
+GO
+
+/****** Object:  Database [DataWarehouse]    Script Date: 6/2/2025 12:50:54 PM ******/
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE [DataWarehouse];
+END;
+GO
+
+/****** Object:  Database [DataWarehouse]    Script Date: 6/2/2025 12:50:54 PM ******/
+CREATE DATABASE [DataWarehouse]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DataWarehouse', FILENAME = N'[Local System Path]\DataWarehouse.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DataWarehouse_log', FILENAME = N'[Local System Path]\DataWarehouse_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+ALTER DATABASE [DataWarehouse] SET COMPATIBILITY_LEVEL = 130
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DataWarehouse].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [DataWarehouse] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [DataWarehouse] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [DataWarehouse] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET  ENABLE_BROKER 
+GO
+
+ALTER DATABASE [DataWarehouse] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [DataWarehouse] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [DataWarehouse] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [DataWarehouse] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [DataWarehouse] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [DataWarehouse] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [DataWarehouse] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [DataWarehouse] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [DataWarehouse] SET QUERY_STORE = OFF
+GO
+
+USE [DataWarehouse]
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
+GO
+
+ALTER DATABASE [DataWarehouse] SET  READ_WRITE 
+GO
+
+-- CREATING SCHEMAS
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
 
